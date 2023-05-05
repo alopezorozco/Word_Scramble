83e656e62205b735e7e93fbddd9c920f82848830
alopezorozco <alopezorozco@uaz.edu.mx>
2023-05-05 09:10:35 -0600

    Commit que actualiza el layout activity_adivinar_palabra.xml

--- app/src/main/java/com/example/wordscramble/GlobalVariables.java
+++ app/src/main/java/com/example/wordscramble/GlobalVariables.java
@@ -2,5 +2,5 @@ package com.example.wordscramble;
 
 
 public class GlobalVariables {
-    public static final String URL = "http://192.168.1.79:8080/";
+    public static final String URL = "http://192.168.1.100:8080/";
 }
